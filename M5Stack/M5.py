ac46c6a73d2abd732f1b1c3baccbdae2cb12bbd1
294471525 <40333098+294471525@users.noreply.github.com>
2020-05-21 18:02:45 +0100

    Added note

--- M5Stack/M5.py
+++ M5Stack/M5.py
@@ -4,12 +4,10 @@ from uiflow import *
 import json
 
 from m5mqtt import M5mqtt
-
-
-
+#Create mqtt service
 m5mqtt = M5mqtt('M5Stack', 'broker.mqttdashboard.com', 1883, '', '', 300)
 setScreenColor(0xffffff)
-
+#Create UI
 title0 = M5Title(title="PokeFood", x=3 , fgcolor=0xFFFFFF, bgcolor=0x000000)
 orderID = M5TextBox(60, 50, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
 rectangle0 = M5Rect(210, 205, 75, 30, 0x4177c3, 0xFFFFFF)
@@ -19,13 +17,14 @@ accept = M5TextBox(46, 210, "Accept", lcd.FONT_Default,0xffffff, rotate=0)
 cancel = M5TextBox(134, 210, "Cancel", lcd.FONT_Default,0xffffff, rotate=0)
 prepared = M5TextBox(216, 210, "Prepared", lcd.FONT_Default,0xFFFFFF, rotate=0)
 items = M5TextBox(60, 85, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
-telephone = M5TextBox(60, 120, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
-status = M5TextBox(60, 155, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
+quantity = M5TextBox(60, 115, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
+telephone = M5TextBox(60, 145, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
+status = M5TextBox(60, 175, "Text", lcd.FONT_Default,0xFFFFFF, rotate=0)
 image0 = M5Img(85, 98, "res/pokefood.jpg", True)
 isProcessed = True
 jsonObject = None
 
-
+#Subscribe to topics
 def fun_PokeFood__M5Sub_(topic_data):
   global jsonObject
   isProcessed = False
@@ -35,6 +34,8 @@ def fun_PokeFood__M5Sub_(topic_data):
   orderID.setColor(0x000000)
   items.setText(str("Items: " + jsonObject['order_items']))
   items.setColor(0x000000)
+  quantity.setText(str("Items: " + jsonObject['order_quantity']))
+  quantity.setColor(0x000000)
   telephone.setText(str("telephone: " + jsonObject['delivery_id']))
   telephone.setColor(0x000000)
   status.setText(str("Status: waiting to be processed"))
@@ -44,7 +45,8 @@ def fun_PokeFood__M5Sub_(topic_data):
 
 if isProcessed :
   m5mqtt.subscribe(str('PokeFood\\M5Sub'), fun_PokeFood__M5Sub_)
-
+  
+#Accept the order
 def buttonA_wasPressed():
   global jsonObject
   accept.setColor(0x000000)
@@ -57,6 +59,7 @@ def buttonA_wasPressed():
   pass
 btnA.wasPressed(buttonA_wasPressed)
 
+#Cancel the order
 def buttonB_wasPressed():
   global jsonObject
   cancel.setColor(0x000000)
@@ -68,12 +71,14 @@ def buttonB_wasPressed():
   wait(5)
   orderID.setColor(0xffffff)
   items.setColor(0xffffff)
+  quantity.setColor(0xffffff)
   telephone.setColor(0xffffff)
   status.setColor(0xffffff)
   image0.show()
   pass
 btnB.wasPressed(buttonB_wasPressed)
 
+#Feedback the customer that the order is ready
 def buttonC_wasPressed():
   global jsonObject
   isProcessed = True
@@ -86,6 +91,7 @@ def buttonC_wasPressed():
   wait(5)
   orderID.setColor(0xffffff)
   items.setColor(0xffffff)
+  quantity.setColor(0xffffff)
   telephone.setColor(0xffffff)
   status.setColor(0xffffff)
   image0.show()
